b9123421cea73176c6286cca4bfc757308624f5b
Jezreel Joshua <jezreeljoshuam@gmail.com>
2022-07-03 21:04:24 +0800

    set size of navbar on different screens

--- src/components/NavigationBar.vue
+++ src/components/NavigationBar.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts"></script>
 
 <template>
-  <div class="flex flex-col bg-indigo-500 h-screen min-w-1/3 md:w-1/6">
+  <div class="flex flex-col bg-indigo-500 h-screen min-w-1/6 md:w-1/4">
     <div>
       <router-link to="/add">Add book</router-link>
     </div>
