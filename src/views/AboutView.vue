faeec1ec3d2ab14dc5fec087c87e081fe0680402
Jezreel Joshua <jezreeljoshuam@gmail.com>
2022-07-03 21:02:37 +0800

    add content in about page

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,7 +1,12 @@
 <script setup lang="ts"></script>
 
 <template>
-  <div class="flex flex-col justify-center items-center">
-    <h1 class="text-9xl">About</h1>
+  <div class="flex flex-col items-center w-screen p-10">
+    <h1 class="text-5xl lg:text-6xl">About</h1>
+
+    <p class="mt-6 text-base lg:text-2xl">
+      LRS - Learning Record system is a simple book management system developed
+      for Gordon Heights National High School
+    </p>
   </div>
 </template>
